a3288a50bc0b3635431fc49e0e26c5ba05954d49
aswarthedoctor@gmail.com <aswarsumarlin12@gituhub.com>
2021-02-18 14:50:38 +0800

    kategori
    belumpi selesai

--- admin/layout/sidebar.php
+++ admin/layout/sidebar.php
@@ -50,9 +50,9 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
+                <a href="?module=produk" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
+                  <p>Kategori</p>
                 </a>
               </li>
             </ul>
---
+++ admin/module/kategori/kategori.php
@@ -0,0 +1,90 @@
+<?php
+$aksi="module/kategori/aksi_kategori.php";
+
+	switch($_GET[act])
+	{
+
+	default:
+		// Tampil Data - mengambil file kategorishow.php
+		echo"<a href='?module=kategori&act=tambahdata'> Tambah </a>
+		 <table id='aswar' class='table table-striped table-bordered 'cellspacing='0' width='150%'>
+		 <thead>
+			<tr>
+				<th>NO</th> <th>id kategori</th> <th>Nama kategori</th><th>Aksi</th>
+			
+			</tr></thead>";
+			$no=0;
+			
+			$data = mysqli_query($konek,"SELECT * FROM kategori");
+			while($r = mysqli_fetch_array($data))
+			{
+				$no++;
+		echo"<tr>
+				<td>$no</td> <td>$r[id_kategori]</td> <td>$r[id_kategori]</td> <td>$r[nm_kategori]</td> <td>$r[deskripsi]</td> <td>$r[gambar]</td> <td>$r[ukuran]</td> <td>$r[harga]</td>  <td>$r[stok]</td> <td>$r[nm_kategori]</td> 
+				<td> 
+					<a href='?module=kategori&act=edituser&id=$r[id_kategori]'> Edit </a> | 
+					<a href='$aksi?module=kategori&act=hapus&id=$r[id_kategori]'> Hapus </a>
+				</td>
+			</tr>";
+			}
+		echo"</table>";
+	break;
+
+	// Tambah Data - memanggil file kategorifm.php
+	case "tambahdata":
+		echo"<form action='$aksi?module=kategori&act=input' method='POST'>
+			<table class='table table-striped table-bordered'>
+				<tr>
+					<td>id_kategori</td> <td><input class='form-control' type=text name=id_kategori></td>
+				</tr>
+				<tr>
+					<td>id_kategori</td> <td><input class='form-control' type=text name=id_kategori></td>
+				</tr>
+				<tr>
+					<td>Nama Lengkap</td> <td><input  class='form-control' type=text name=nm_lengkap></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<input class='btn btn-default' type=submit name=simpan value='Kirim'>
+						<input class='btn btn-default' type=reset name=batal value='Batal'>
+					</td>
+				</tr>
+			</table>
+		</form>";	
+	break;
+		
+	// Edit Data - memanggil file kategorieditfm.php
+	case "editdata":
+		
+			$data = mysqli_query($konek,"SELECT * FROM kategori where id_kategori='$_GET[id]'");
+			$r = mysqli_fetch_array($data);
+				
+		echo"<form action='$aksi?module=kategori&act=update' method='POST'>
+			<table class='table table-striped table-bordered'>
+				<tr>
+					<td>id_kategori</td> 
+					<td>
+						<input type=text name=id_kategori value='$r[id_kategori]' disabled>
+						<input type=hidden name='idh' value='$r[id_kategori]'>
+					</td>
+				</tr>
+				<tr>
+					<td>id_kategori</td> <td><input class='form-control' type=id_kategori name=id_kategori value='$r[id_kategori]'></td>
+				</tr>
+				<tr>
+					<td>Nama Lengkap</td> <td><input class='form-control' type=text name=nm_lengkap value='$r[nm_lengkap]'></td>
+				</tr>
+				<tr>
+					<td></td> 
+					<td>
+						<input type=submit class='btn btn-default' name=simpan value='Update'>
+						<input type=reset class='btn btn-default' name=batal value='Batal'>
+					</td>
+				</tr>
+			</table>
+		</form>";
+	break;
+}
+?>
+	  
