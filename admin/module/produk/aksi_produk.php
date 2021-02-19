2722e160a4d9e4095fb513025be6820fb2b39e81
firman4658 <78971161+firman4658@users.noreply.github.com>
2021-02-20 07:46:40 +0800

    perubahn

--- admin/module/produk/aksi_produk.php
+++ admin/module/produk/aksi_produk.php
@@ -8,24 +8,25 @@ else{
 
 	include "../../../config/koneksi.php";
 	$module=$_GET[module];
-	$act=$_GET[act];
+	$act=$_GET['act'];
 
-	//hapus memanggil file produkdel.php
+	//hapus memanggil file kategoridel.php
 	if ($module=='produk' AND $act=='hapus'){
-		mysqli_query($konek,"delete from produk where username='$_GET[id]'");
+		mysqli_query($konek,"delete from produk where id_produk='$_GET[id]'");
 		header('location:../../home.php?module='.$module);
 	}
 
-	//input memanggil file produksim.php
+	//input memanggil file kategorisim.php
 	elseif ($module=='produk' AND $act=='input') {
-		mysqli_query($konek,"INSERT INTO produk values ('$_POST[id_produk]','$_POST[id_kategori]','$_POST[nm_produk]'),'$_POST[deksripsi]'),'$_POST[gambar]'),'$_POST[ukuran]'),'$_POST[ukuran_sepatu]'),'$_POST[harga]'),'$_POST[stok]'),'$_POST[nm_kategori]')");
+		mysqli_query($konek,"INSERT INTO produk values ('$_POST[id_produk]','$_POST[id_kategori]','$_POST[nm_produk]','$_POST[deskripsi]','$_POST[gambar]','$_POST[ukuran]','$_POST[ukuran_sepatu]','$_POST[harga]','$_POST[stok]','$_POST[nm_kategori]')");
 		header('location:../../home.php?module='.$module);
 	}
 
-	//update memanggil file produkeditsim.php
+	//update memanggil file kategorieditsim.php
 	elseif ($module=='produk' AND $act=='update') {
-		mysqli_query($konek,"UPDATE berita SET id_produk='$_POST[id_produk]',id_kategori='$_POST[id_kategori]', nm_produk= '$_POST[nm_produk]',deskripsi='$_POST[deksripsi]',gambar='$_POST[gambar]',ukuran='$_POST[ukuran]',ukuran_sepatu='$_POST[ukuran_sepatu]',harga='$_POST[harga]',stok='$_POST[stok]',nm_kategori='$_POST[nm_kategori]' where id_produk= '$_POST[id]'");
+		mysqli_query($konek,"UPDATE  set id_produk='$_POST[id_produk]',id_kategori='$_POST[id_kategori]',nm_produk='$_POST[nm_produk]',deskripsi='$_POST[deskripsi]',gambar='$_POST[gambar]',ukuran='$_POST[ukuran]',ukuran_sepatu='$_POST[ukuran_sepatu]',harga='$_POST[harga]',stok='$_POST[stok]',nm_kategori='$_POST[nm_kategori]'
+		where id_produk='$_POST[idh]'");
 		header('location:../../home.php?module='.$module);
 	}
-}
+ }
 ?>
