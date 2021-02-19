9aea7a63177dafb853e8fca215b71a896c998d28
firman4658 <78971161+firman4658@users.noreply.github.com>
2021-02-20 07:17:55 +0800

    perubahan aksi

--- admin/module/produk/aksi_produk.php
+++ admin/module/produk/aksi_produk.php
@@ -18,15 +18,13 @@ else{
 
 	//input memanggil file produksim.php
 	elseif ($module=='produk' AND $act=='input') {
-		mysqli_query($konek,"INSERT INTO produk values ('$_POST[]','$_POST[password]','$_POST[nm_lengkap]')");
+		mysqli_query($konek,"INSERT INTO produk values ('$_POST[id_produk]','$_POST[id_kategori]','$_POST[nm_produk]'),'$_POST[deksripsi]'),'$_POST[gambar]'),'$_POST[ukuran]'),'$_POST[ukuran_sepatu]'),'$_POST[harga]'),'$_POST[stok]'),'$_POST[nm_kategori]')");
 		header('location:../../home.php?module='.$module);
 	}
 
 	//update memanggil file produkeditsim.php
 	elseif ($module=='produk' AND $act=='update') {
-		mysqli_query($konek,"UPDATE produk set password='$_POST[password]',nm_lengkap='$_POST[nm_lengkap]'
-		where username='$_POST[idh]'");
+		mysqli_query($konek,"UPDATE berita SET id_produk='$_POST[id_produk]',id_kategori='$_POST[id_kategori]', nm_produk= '$_POST[nm_produk]',deskripsi='$_POST[deksripsi]',gambar='$_POST[gambar]',ukuran='$_POST[ukuran]',ukuran_sepatu='$_POST[ukuran_sepatu]',harga='$_POST[harga]',stok='$_POST[stok]',nm_kategori='$_POST[nm_kategori]' where id_produk= '$_POST[id]'");
 		header('location:../../home.php?module='.$module);
 	}
- }
 ?>
